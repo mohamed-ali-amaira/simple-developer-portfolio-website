433ca9c06a04c6e85155a237b7c6a85b3ea1614a
mohamed-ali-amaira <mohamedaliamaira@gmail.com>
2023-06-18 14:30:58 +0100

    initial commit

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -8,8 +8,8 @@ export default function ContainerBlock({ children, ...customMeta }) {
   const router = useRouter();
 
   const meta = {
-    title: "Manu Arora - Developer, Writer, Creator and YouTuber",
-    description: `I've been developing websites for 5 years straight. Get in touch with me to know more.`,
+    title: "Mohamed Ali Amaira - Developer, Writer, Creator",
+    description: `I am a React.js Rockstar`,
     image: "/avatar.png",
     type: "website",
     ...customMeta,
@@ -18,31 +18,31 @@ export default function ContainerBlock({ children, ...customMeta }) {
     <div>
       <Head>
         <title>{meta.title}</title>
-        <meta name="robots" content="follow, index" />
-        <meta content={meta.description} name="description" />
+        <meta name='robots' content='follow, index' />
+        <meta content={meta.description} name='description' />
         <meta
-          property="og:url"
+          property='og:url'
           content={`https://yourwebsite.com${router.asPath}`}
         />
         <link
-          rel="canonical"
+          rel='canonical'
           href={`https://yourwebsite.com${router.asPath}`}
         />
-        <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Manu Arora" />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@mannupaaji" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
+        <meta property='og:type' content={meta.type} />
+        <meta property='og:site_name' content='Manu Arora' />
+        <meta property='og:description' content={meta.description} />
+        <meta property='og:title' content={meta.title} />
+        <meta property='og:image' content={meta.image} />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:site' content='@mannupaaji' />
+        <meta name='twitter:title' content={meta.title} />
+        <meta name='twitter:description' content={meta.description} />
+        <meta name='twitter:image' content={meta.image} />
         {meta.date && (
-          <meta property="article:published_time" content={meta.date} />
+          <meta property='article:published_time' content={meta.date} />
         )}
       </Head>
-      <main className="dark:bg-gray-800 w-full">
+      <main className='dark:bg-gray-800 w-full'>
         <Navbar />
         <div>{children}</div>
         <Footer />
